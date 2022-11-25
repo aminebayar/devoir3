effaf159469c93a32a0dd185fcdc9b32e13dc10f
aminebayar <minoamine2011@gmail.com>
2022-11-25 19:05:39 +0100

    Add files via upload

---
+++ user-microservice/src/main/java/com/amine/user/security/SecParams.java
@@ -0,0 +1,6 @@
+package com.amine.user.security;
+
+public interface SecParams {
+public static final long EXP_TIME = 10*24*60*60*1000;
+public static final String SECRET = "aminebayar811@gmail.com";
+}
---
+++ user-microservice/src/main/java/com/amine/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.amine.user.service;
+
+import com.amine.user.entities.Role;
+import com.amine.user.entities.User;
+public interface UserService {
+User saveUser(User user);
+User findUserByUsername (String username);
+Role addRole(Role role);
+User addRoleToUser(String username, String rolename);
+}
